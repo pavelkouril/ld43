70516a64349a42a15fb0fda5707cfd253d5c4422
Pavel Kouřil <pk@pavelkouril.cz>
2018-12-02 09:44:40 +0100

    added dummy envelope to balloon prefab

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -5,8 +5,39 @@ public class UiManager : MonoBehaviour
 {
     public Text GameEnd;
 
+    public GameObject Passenger1;
+    public GameObject Passenger2;
+    public GameObject Passenger3;
+    public GameObject Passenger4;
+    public GameObject Passenger5;
+    public GameObject Passenger6;
+
     public void SetGameEndText(string text)
     {
         GameEnd.text = text;
+    }
+
+    public void Throw1()
+    {
+    }
+
+    public void Throw2()
+    {
+    }
+
+    public void Throw3()
+    {
+    }
+
+    public void Throw4()
+    {
+    }
+
+    public void Throw5()
+    {
+    }
+
+    public void Throw6()
+    {
     }
 }

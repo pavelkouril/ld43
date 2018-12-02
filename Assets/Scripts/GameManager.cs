7ebbe6aae716ed2a8177bf17d960407875966e2d
Pavel Kouřil <pk@pavelkouril.cz>
2018-12-02 02:08:26 +0100

    added basic ui stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,15 +6,24 @@ public struct Passenger
     public string Name;
     public int NetWorth;
     public int Weight;
+
+    public Passenger(string name, int netWorth, int weight)
+    {
+        Name = name;
+        NetWorth = netWorth;
+        Weight = weight;
+    }
 }
 
 public class GameManager : MonoBehaviour
 {
     private List<Passenger> _passengers = new List<Passenger>();
 
-    void Start()
+    private void Start()
     {
-
+        _passengers.Add(new Passenger("X", 120000000, 120));
+        _passengers.Add(new Passenger("Y", 1000000, 80));
+        _passengers.Add(new Passenger("Z", 1111111, 55));
     }
 
     void Update()
---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public Text GameEnd;
+
+    public void SetGameEndText(string text)
+    {
+        GameEnd.text = text;
+    }
+}
